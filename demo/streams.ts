07db20272ed071f3ed0faa3b9ec482f1f42f28a3
johnpyp <20625636+johnpyp@users.noreply.github.com>
2020-10-25 19:39:39 -0400

    Add streams demo

---
+++ demo/streams.ts
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import Client from "../src/client";
+
+const main = async (): Promise<void> => {
+  const client = new Client({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+  });
+  try {
+    const user = await client.getUser("destiny");
+    console.log("User", user!.json());
+    const stream = await user!.getStream();
+    console.log("Stream", stream!.json());
+  } catch (error) {
+    console.log("Error", error, error?.response?.body);
+  }
+};
+
+main();
--- test/client.test.ts
+++ test/client.test.ts
@@ -1,16 +1,16 @@
 import Client from "../src/client";
 
-it("client wurks", async () => {
-  const client = new Client({
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
+describe("client", () => {
+  it("client wurks", async () => {
+    const client = new Client({
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+    });
+    try {
+      const meme = await client.getUser("destiny");
+      expect(meme.displayName).toStrictEqual("Destiny");
+    } catch (error) {
+      console.log("Error", error);
+    }
   });
-  try {
-    const meme = await client.getUser("destiny");
-    console.log(meme);
-    expect(meme.raw).toStrictEqual("hello");
-  } catch (error) {
-    console.log("Error", error);
-    // console.log(error.response.body);
-  }
 });
